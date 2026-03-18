f7627f49ea075be7d36a2e2562384e92768675cd
santiago <a01255853@tec.mx>
2026-03-17 18:14:31 -0700

    feat(routes): add app layout with navbar and nested routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,10 +1,20 @@
 import { createBrowserRouter } from 'react-router';
+import App from '../App';
+
+const HomePage = () => <h1 className='text-2xl font-bold'>Inicio</h1>
+const TierPage = () => <h1 className='text-2xl font-bold'>Tiers</h1>
+const EmpresaMiembroPage = () => <h1 className="text-2xl font-bold">Empresas Miembro</h1>
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Home</div>
-  }
+    element: <App />,
+    children: [
+        { index: true, element: <HomePage /> },
+        { path: 'tier', element: <TierPage /> },
+        { path: 'empresa-miembro', element: <EmpresaMiembroPage /> }
+    ],
+  },
 ]);
 
 export default router;
